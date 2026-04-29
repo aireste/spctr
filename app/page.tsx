757a517ac19721c2f66c797b3b6bcaab3eefb3a1
aireste <estebanjguerra@gmail.com>
2026-04-28 21:56:37 -0500

    add LinkedIn link to footer

--- app/page.tsx
+++ app/page.tsx
@@ -83,6 +83,7 @@ export default function Home() {
           <li><a href="#">Privacy</a></li>
           <li><a href="#">Terms</a></li>
           <li><a href="#">Contact</a></li>
+          <li><a href="https://www.linkedin.com/in/esteban-guerra-b3631415b/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
         </ul>
       </footer>
     </main>
